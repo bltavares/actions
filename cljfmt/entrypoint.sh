386fcd22fe28433fc4de74c17a1ab44816a88eb6
Bruno Tavares <connect+github@bltavares.com>
2019-01-22 10:01:27 -0200

    Pull Request Review

--- cljfmt/entrypoint.sh
+++ cljfmt/entrypoint.sh
@@ -10,7 +10,7 @@ lint() {
     lein cljfmt check
 }
 
-_should_fix() {
+_should_fix_issue() {
     pr_url="$(jq --raw-output '.issue.pull_request.url | select(. != null)' "$GITHUB_EVENT_PATH")"
     fix_comment="$( jq --raw-output '.comment.body | select(. | startswith("lint fix"))' "$GITHUB_EVENT_PATH")"
     [[ -n "$pr_url" ]] && [[ -n "$fix_comment" ]] || exit 0
@@ -32,6 +32,11 @@ _commit_if_needed() {
     fi
 }
 
+_should_fix_review() {
+    fix_comment="$( jq --raw-output '.review.body | select(. | startswith("lint fix"))' "$GITHUB_EVENT_PATH")"
+    [[ -n "$fix_comment" ]] || exit 0
+}
+
 main() {
     [[ -z "$GITHUB_TOKEN" ]] && echo "Set the GITHUB_TOKEN env variable." && exit 1
 
@@ -39,8 +44,13 @@ main() {
         lint
     elif [[ "$GITHUB_EVENT_NAME" == "issue_comment" ]]; then
         set -x
-        _should_fix
+        _should_fix_issue
         _switch_to_branch
+        fix
+        _commit_if_needed
+    elif [[ "$GITHUB_EVENT_NAME" == "pull_request_review" ]]; then
+        set -x
+        _should_fix_review
         fix
         _commit_if_needed
     fi
