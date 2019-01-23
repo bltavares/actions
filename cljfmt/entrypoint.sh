acdd27cdd841368e90f86ab89109dd49e2c61b9d
Bruno Tavares <connect+github@bltavares.com>
2019-01-23 12:22:23 -0200

    Allow using the action name or fallback to cljfmt

--- cljfmt/entrypoint.sh
+++ cljfmt/entrypoint.sh
@@ -19,12 +19,12 @@ main() {
         lint
     elif [[ "$GITHUB_EVENT_NAME" == "pull_request_review" ]]; then
         _requires_token
-        _should_fix_review "fix cljfmt"
+        _should_fix_review "fix $GITHUB_ACTION" || _should_fix_review "fix cljfmt"
         fix
         _commit_if_needed
     elif [[ "$GITHUB_EVENT_NAME" == "TODO_issue_comment" ]]; then
         _requires_token
-        _should_fix_issue "fix cljfmt"
+        _should_fix_issue "fix $GITHUB_ACTION" || _should_fix_issue "fix cljfmt"
         # TODO: I'm unable to get the branch given an issue comment event
         _switch_to_branch
         fix
