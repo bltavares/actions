a4feb67173e1f17f11d86086bfe9606c8d76443e
Bruno Tavares <connect+github@bltavares.com>
2019-01-25 12:55:15 +0000

    Debug

--- dartfmt/entrypoint.sh
+++ dartfmt/entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x
 
 # shellcheck disable=SC1091
 source /lib.sh
 
 fix() {
-	dartfmt -w --fix .
+  dartfmt -w --fix .
 }
 
 lint() {
-	dartfmt -n --fix . && dart-analyzer
+  dartfmt -n --fix . && dart-analyzer
 }
 
 _lint_and_fix_action dartfmt "${@}"
