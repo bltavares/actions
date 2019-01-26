f53f57ff491aef6cc0b496d518fe274f3069eb28
Bruno Tavares <connect+github@bltavares.com>
2019-01-26 15:05:34 +0000

    Remove debugs

--- dartfmt/entrypoint.sh
+++ dartfmt/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 set -euo pipefail
-set -x
 
 # shellcheck disable=SC1091
 source /lib.sh
 
 fix() {
-	dartfmt -w --fix .
+  dartfmt -w --fix .
 }
 
 lint() {
-	dartfmt -n --fix .
+  dartfmt -n --fix .
 }
 
 _lint_and_fix_action dartfmt "${@}"
