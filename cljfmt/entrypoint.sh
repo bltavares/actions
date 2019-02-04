5a1a096cdf7b592f5bca2a0a6e6e00e752b98b77
Bruno Tavares <connect+github@bltavares.com>
2019-02-04 13:21:05 -0200

    shfmt: lint fix

--- cljfmt/entrypoint.sh
+++ cljfmt/entrypoint.sh
@@ -7,26 +7,26 @@ source /lib.sh
 declare -a file_args=()
 
 cljfmt() {
-    clojure -Sdeps '{:deps {lein-cljfmt {:mvn/version "0.6.4"}}}' \
-            -m cljfmt.main "${file_args[@]}" "$@"
+	clojure -Sdeps '{:deps {lein-cljfmt {:mvn/version "0.6.4"}}}' \
+		-m cljfmt.main "${file_args[@]}" "$@"
 }
 
 fix() {
-    cljfmt fix
+	cljfmt fix
 }
 
 lint() {
-    cljfmt check
+	cljfmt check
 }
 
 setup_files() {
-    if [[ -f .cljfmt-indents.edn ]]; then
-        file_args+=(--indents .cljfmt-indents.edn)
-    fi
+	if [[ -f .cljfmt-indents.edn ]]; then
+		file_args+=(--indents .cljfmt-indents.edn)
+	fi
 
-    if [[ -f .cljfmt-alias.edn ]]; then
-        file_args+=(--alias-map .cljfmt-alias.edn)
-    fi
+	if [[ -f .cljfmt-alias.edn ]]; then
+		file_args+=(--alias-map .cljfmt-alias.edn)
+	fi
 }
 
 setup_files
