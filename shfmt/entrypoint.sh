52eb800014ae8ed3053381004d149c53eb2e16d5
Bruno Tavares <connect+github@bltavares.com>
2019-08-18 10:10:45 -0300

    Cleanup shfmt entrypoint

--- shfmt/entrypoint.sh
+++ shfmt/entrypoint.sh
@@ -5,16 +5,12 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source /lib.sh
 
-if [ -z "${SHFMT_ARGS:-}" ]; then
-	SHFMT_ARGS=""
-fi
-
 fix() {
 	shfmt -s -w ${SHFMT_ARGS:-} .
 }
 
 lint() {
-	shfmt -s -l -d ${SHFMT_ARGS} .
+	shfmt -s -l -d ${SHFMT_ARGS:-} .
 }
 
 _lint_and_fix_action shfmt "${@}"
