7ba35c89e03017ce2dee392648d1a73343677ea7
Felipe Cortez <felipecortezfi@gmail.com>
2019-11-21 17:54:37 -0300

    Tabs, not spaces

--- cljfmt/entrypoint.sh
+++ cljfmt/entrypoint.sh
@@ -6,25 +6,25 @@ set -euo pipefail
 source /lib.sh
 
 rewrite() {
-  clojure -Sdeps '{:deps {rewrite-clj {:mvn/version "0.6.1"}}}' /rewrite_projectclj.clj
+	clojure -Sdeps '{:deps {rewrite-clj {:mvn/version "0.6.1"}}}' /rewrite_projectclj.clj
 }
 
 cljfmt() {
-  rewrite
-  lein cljfmt "$@"
-  git checkout -- project.clj
+	rewrite
+	lein cljfmt "$@"
+	git checkout -- project.clj
 }
 
 fix() {
-  rewrite
-  lein cljfmt fix
-  git checkout -- project.clj
+	rewrite
+	lein cljfmt fix
+	git checkout -- project.clj
 }
 
 lint() {
-  rewrite
+	rewrite
 	lein cljfmt check
-  git checkout -- project.clj
+	git checkout -- project.clj
 }
 
 _lint_and_fix_action cljfmt "${@}"
