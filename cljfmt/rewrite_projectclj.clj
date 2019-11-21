bdab3878ea061e2edd556ee9333d6f763d2c894f
Felipe Cortez <felipecortezfi@gmail.com>
2019-11-21 18:05:06 -0300

    Move rewrite_projectclj's destructuring to defn

--- cljfmt/rewrite_projectclj.clj
+++ cljfmt/rewrite_projectclj.clj
@@ -2,13 +2,12 @@
   (:require [rewrite-clj.zip :as z]))
 
 (defn only-plugins-and-cljfmt
-  [sexpr]
-  (let [[defproj proj version & {:keys [plugins cljfmt test-paths]}] sexpr]
-    (list
-     defproj proj version
-     :plugins    (filterv (fn [[k v]] (= k 'lein-cljfmt)) plugins)
-     :cljfmt     cljfmt
-     :test-paths test-paths)))
+  [[defproj proj version & {:keys [plugins cljfmt test-paths]}]]
+  (list
+   defproj proj version
+   :plugins    (filterv (fn [[k v]] (= k 'lein-cljfmt)) plugins)
+   :cljfmt     cljfmt
+   :test-paths test-paths))
 
 (-> (z/of-file "/github/workspace/project.clj")
     (z/edit only-plugins-and-cljfmt)
